4b3187f54dccaaf71364fb99c56107737cec7722
GurritoN <gurritoglebon@gmail.com>
2018-07-17 18:23:25 +0300

    Minor changes in assembly info

--- RestrauntQueueSim/Properties/AssemblyInfo.cs
+++ RestrauntQueueSim/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OOP3")]
+[assembly: AssemblyTitle("RestrauntQueueSim")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OOP3")]
+[assembly: AssemblyProduct("RestrauntQueueSim")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
